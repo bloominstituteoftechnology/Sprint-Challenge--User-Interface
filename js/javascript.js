d72a31a38da2b18102e2fa3ad29eb2e98e35f283
iAmAdamReid <adam@adamreid.me>
2018-07-27 10:43:18 -0700

    Adam Reid - UI Sprint - Sticky Navbar

---
+++ js/javascript.js
@@ -0,0 +1,47 @@
+
+// // enables responsive menu toggle
+// $(document).ready(function () {
+//     $('#collapse-btn').click(function () {
+//         if ($('#navigation').hasClass('navbar')) {
+//             $('#navigation').removeClass('navbar');
+//             $('#navigation').addClass('navbar-open');
+//         } else {
+//             $('#navigation').removeClass('navbar-open');
+//             $('#navigation').addClass('navbar');
+//         }
+//     });
+
+// });
+
+// Sticky navbar javascript
+$(document).ready(function () {
+    window.onscroll = function () {
+        stickyNav();
+    }
+
+    var navbar = document.getElementById('navigation');
+    var offset = navbar.offsetTop;
+
+    function stickyNav() {
+        if (window.pageYOffset >= offset) {
+            navbar.classList.add('sticky');
+        } else {
+            navbar.classList.remove('sticky');
+        }
+    }
+})
+
+
+
+
+// // Run a continuous check for the page width
+// // This prevents the responsive menu from staying open
+// // if page resizes to larger than 899px
+// $(document).ready(function() {
+//     setInterval(function(){
+//         if($(window).width() > 899){
+//             $('#navigation').removeClass('navbar-open');
+//             $('#navigation').addClass('navbar');
+//         }
+//     }, 100);
+// });
