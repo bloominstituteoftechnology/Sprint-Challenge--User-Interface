cb31d8d8e875e9cb03fe8499a91644056605b7d5
Mike Nichols <mikenichols410@gmail.com>
2019-06-09 22:01:00 -0400

    Almost finished with the about page....

---
+++ index.js
@@ -0,0 +1,54 @@
+// let memory = document.querySelector('main').innerHTML;
+// memory;
+// document.querySelectorAll('ul li a')
+// document.querySelector('main').innerHTML = `  <img class="flex-inline flex-justify-around" src="img/jumbo.jpg">
+// <div class="flex flex-justify-between">
+//   <div>
+//     <h2 class="u-margin-bottom-sm">The Future</h2>
+//     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione animi corporis pariatur modi? Animi numquam
+//       dolor quos modi dicta! Cumque, dolore quidem. Dolorum explicabo natus alias perspiciatis eum quas porro?</p>
+//   </div>
+//   <div>
+//     <h2 class="u-margin-bottom-sm">The Past</h2>
+//     <p>Omnis maxime magnam laudantium neque. Minus, temporibus. Laborum beatae officiis soluta necessitatibus dolorum.
+//       Id, repellendus maiores voluptatem quaerat totam consectetur, obcaecati dolore earum aspernatur a modi eveniet
+//       ipsum adipisci impedit.</p>
+//   </div>
+// </div>
+// <hr>
+// <h2>Why Did It Have To Be Boxes...</h2>
+// <div class="flex flex-wrap flex-justify-around">
+//   <div class="box box-1">Box 1</div>
+//   <div class="box box-2">Box 2</div>
+//   <div class="box box-3">Box 3</div>
+//   <div class="box box-4">Box 4</div>
+//   <div class="box box-5">Box 5</div>
+//   <div class="box box-6">Box 6</div>
+//   <div class="box box-7">Box 7</div>
+//   <div class="box box-8">Box 8</div>
+//   <div class="box box-9">Box 9</div>
+//   <div class="box box-10">Box 10</div>
+// </div>
+// <hr>
+// <div class="flex flex-justify-around">
+//   <div>
+//     <h3 class="u-margin-bottom-sm">That's</h3>
+//     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, laudantium quaerat doloremque quis, est
+//       velit soluta eligendi magnam nam rem voluptates? Ducimus, asperiores saepe? Facilis dolore veniam consequuntur
+//       obcaecati? Perferendis?</p>
+//   </div>
+//   <div>
+//     <h3 class="u-margin-bottom-sm">No</h3>
+//     <p>Asperiores vitae molestias incidunt provident velit, facere expedita iusto culpa non perferendis quod dolorum
+//       laboriosam. Atque nam itaque recusandae perferendis, dolore esse, veritatis necessitatibus voluptas labore
+//       repudiandae voluptatum accusantium! Corrupti.</p>
+//   </div>
+//   <div>
+//     <h3 class="u-margin-bottom-sm">Moon</h3>
+//     <p>Voluptatem itaque consequuntur reiciendis nulla odio inventore voluptate voluptas, modi mollitia dolore?
+//       Commodi, illo perferendis? Illum, nam commodi, consequatur impedit animi veritatis ratione possimus cumque
+//       dolore totam blanditiis in perferendis?</p>
+//   </div>
+// </div>
+// </div>
+// </div>`;
